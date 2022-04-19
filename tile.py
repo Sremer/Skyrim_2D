019b597b139191a5768e9323c1d97134f0108a91
seth <seth.e.remer@gmail.com>
2022-04-19 02:11:27 -0600

    Player Animation

--- tile.py
+++ tile.py
@@ -5,6 +5,11 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
+        self.sprite_type = 'grass'
+        y_offset = HITBOX_OFFSET[self.sprite_type]
         self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, -10)
+        if self.sprite_type == 'object':
+            self.rect = self.image.get_rect(topleft=(pos[0], pos[1] - TILESIZE))
+        else:
+            self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, y_offset)
