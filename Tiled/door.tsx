a3ec7ac811336d7c1aa70682dd11fc677185e3dd
seth <seth.e.remer@gmail.com>
2022-07-01 00:12:39 -0700

    Area Logic

---
+++ Tiled/door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="door" tilewidth="64" tileheight="64" tilecount="2346" columns="51">
+ <image source="../../../Documents/Skyrim Graphics/graphics/background/door_tileset.png" width="3300" height="2972"/>
+</tileset>
